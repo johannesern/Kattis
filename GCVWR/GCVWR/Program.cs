cdb0d3db5cd59256a274f3746844493e04c4a9ef
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-02 21:17:16 +0200

    GCVWR solution added

---
+++ GCVWR/GCVWR/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GCVWR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[]GCVWRtruckItems = MakeStringToIntArray(input);
+            string weightItems = Console.ReadLine();
+            int[] itemsSeparatedAsInt = MakeStringToIntArray(weightItems);
+            float ninetyPercent = 0.9f;
+            float GminusT = (GCVWRtruckItems[0] - GCVWRtruckItems[1]) * ninetyPercent;
+            int itemsSum = 0;
+            for (int i = 0; i < itemsSeparatedAsInt.Length; i++)
+            {
+                itemsSum += itemsSeparatedAsInt[i];
+            }
+            float weightOfTrailer;
+            Console.WriteLine(weightOfTrailer = GminusT - itemsSum); 
+        }
+        public static int[] MakeStringToIntArray(string input)
+        {
+            string[] inputAsArr = input.Split(" ");
+            int[] inputAsIntArr = new int[inputAsArr.Length];
+            for (int i = 0; i < inputAsArr.Length; i++)
+            {
+                inputAsIntArr[i] = int.Parse(inputAsArr[i]);
+            }
+            return inputAsIntArr;
+        }
+    }
+}
