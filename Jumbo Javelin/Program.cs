1cab8e12c34ef8deb767bbd03064b3325cc8d02f
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-02 14:54:13 +0200

    here are som solutions

---
+++ Jumbo Javelin/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace KATTIS_Jumbo_Javelin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32.TryParse(Console.ReadLine(), out int numberOfRods);
+            int counter = 0;
+            int totalRodLength = 0;
+            
+            Int32.TryParse(Console.ReadLine(), out int firstRod);
+            while (counter < (numberOfRods - 1))
+            {
+                Int32.TryParse(Console.ReadLine(), out int nRod);            
+                firstRod += nRod - 1;
+                totalRodLength = firstRod;
+                counter++;
+            }
+            Console.WriteLine(totalRodLength);
+        }
+    }
+}
