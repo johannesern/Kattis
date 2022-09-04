15586ea2960e4dd5e5cbed3975c828ac2c39e5d1
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-04 11:03:05 +0200

    Filip sln added

---
+++ Filip/Filip/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Filip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            string input = Console.ReadLine();
+            char[] cInput = input.ToCharArray();
+            Array.Reverse(cInput);
+            input = string.Concat(cInput);
+            string[] inputArr = input.Split(" ");
+            if (int.Parse(inputArr[0]) < int.Parse(inputArr[1]))
+            {
+                Console.WriteLine(inputArr[1]);
+            }
+            else
+            {
+                Console.WriteLine(inputArr[0]);
+            }
+        }
+    }
+}
