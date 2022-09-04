7a545a97b5ef0a1cecd3be40aa61f0365c79a4c7
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-04 14:51:39 +0200

    Eye of Sauron sln added

---
+++ Eye of Sauron/Eye of Sauron/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Eye_of_Sauron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sauronsEyeInMid = Console.ReadLine();
+            int midOfSentence = Convert.ToInt32(sauronsEyeInMid.Length / 2);
+            if(Convert.ToInt32(sauronsEyeInMid.Length) % 2 == 0)
+            {
+                if (sauronsEyeInMid.IndexOf("(") + 1 == midOfSentence)
+                {
+                    Console.WriteLine("correct");
+                }
+                else
+                {
+                    Console.WriteLine("fix");
+                }
+            }
+            else
+            {
+                Console.WriteLine("fix");
+            }
+        }
+    }
+}
