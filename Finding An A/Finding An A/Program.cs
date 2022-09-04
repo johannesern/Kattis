96f0ed1eaf7823bc1afe6836638fa918c8993ab5
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-04 09:37:55 +0200

    Finding An A solution added

---
+++ Finding An A/Finding An A/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Finding_An_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int m = input.Length;
+            int indexOfA = input.IndexOf("a");
+            string n = input.Substring(indexOfA, m - indexOfA);
+            Console.WriteLine(n);
+        }
+    }
+}
