02a78d5b83be132ec5edfe735e73eca9f18d757f
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-05 11:11:10 +0200

    A Different Problem sln

---
+++ A Different Problem/A Different Problem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace A_Different_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            string line;
+            string[] temp;
+            long[] longs = new long[2];
+            while((line = Console.ReadLine()) != null)
+            {
+                temp = line.Split(" ");
+                for (int i = 0; i < longs.Length; i++)
+                {
+                    longs[i] = long.Parse(temp[i]);
+                }
+                if (longs[0] < longs[1])
+                {
+                    long res = longs[1] - longs[0];
+                    Console.WriteLine(res);
+                }
+                else
+                {
+                    long res = longs[0] - longs[1];
+                    Console.WriteLine(res);
+                }
+            }
+        }
+    }
+}
