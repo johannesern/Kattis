2a03f326e2c8007f2e09ba84bff74fd86ad873b6
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-08 07:14:09 +0200

    added ABC

---
+++ ABC/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ABC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] oldOrder = Console.ReadLine().Split(" ").ToArray();
+            string ABC = Console.ReadLine();
+            Array.Sort(oldOrder);
+            int A = int.Parse(oldOrder[0]);
+            int B = int.Parse(oldOrder[1]);
+            int C = int.Parse(oldOrder[2]);
+            int[] newOrder = new int[oldOrder.Length];
+            newOrder[ABC.IndexOf('A')] = A;
+            newOrder[ABC.IndexOf('B')] = B;
+            newOrder[ABC.IndexOf('C')] = C;
+            foreach (int i in newOrder)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
